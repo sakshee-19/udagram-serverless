6d3129fbc0ce57317503da22fde76220e1f72274
saksjain <saksjain@adobe.com>
2020-05-17 18:00:26 +0530

    add feat to get image with imageId using GSI

---
+++ src/lambda/http/getImage.ts
@@ -0,0 +1,44 @@
+import {APIGatewayProxyHandler, APIGatewayProxyResult, APIGatewayProxyEvent} from 'aws-lambda';
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const imageIndex = process.env.IMAGES_INDEX;
+const imageTable = process.env.IMAGES_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent ): Promise<APIGatewayProxyResult> => {
+    console.log("processing event ", event);
+
+    const imageId = event.pathParameters.imageId;
+
+    const results = await getImages(imageId)
+
+    if(results.Count <= 0) {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({
+                message: 'immge id does not exists.'
+            })
+        }
+    }
+    console.log(results)
+
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(results.Items[0])
+    }
+}
+
+async function getImages(imageId) {
+    const result = await docClient.query({
+        TableName: imageTable,
+        IndexName: imageIndex,
+        KeyConditionExpression: 'imageId = :imageId',
+        ExpressionAttributeValues: {
+            ':imageId': imageId
+        }
+    }).promise()
+    return result
+}
