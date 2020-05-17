04910ca81a050ab369a8dbb6ba90211763b25dbc
saksjain <saksjain@adobe.com>
2020-05-18 01:40:06 +0530

    add api to upload image to s3 bucket

--- src/lambda/http/createImage.ts
+++ src/lambda/http/createImage.ts
@@ -4,8 +4,14 @@ import * as uuid from 'uuid';
 
 const imageTable = process.env.IMAGES_TABLE
 const groupTable = process.env.GROUPS_TABLE
+// const expirationTime = process.env.SIGNED_URL_EXPIRATION
+const bucketName = process.env.IMAGES_S3_BUCKET
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+
+const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+})
 
 export const handler:APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log("processing event ",event);
@@ -28,15 +34,18 @@ export const handler:APIGatewayProxyHandler = async (event: APIGatewayProxyEvent
     const item = JSON.parse(event.body)
 
     const imageId = uuid.v4();
+    const imageUrl = `https://${bucketName}.s3.ap-south-1.amazonaws.com/${imageId}` // save url in db as this
 
     var date = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
     // var timestamp = date.toString()
+    const url = getSignedUrl(imageId) //url returned while createion to let user upload image for the same info of image
 
     const newItem = {
         groupId: groupItem.id,
         imageId: imageId,
         timestamp: date.toString(),
-        ...item
+        ...item,
+        imageUrl: imageUrl
     }
     console.log("new item ", newItem)
 
@@ -50,7 +59,10 @@ export const handler:APIGatewayProxyHandler = async (event: APIGatewayProxyEvent
         headers: {
             "Access-Cotrol-Allow_origin":"*"
         },
-        body: JSON.stringify(newItem)
+        body: JSON.stringify({
+            newItem: newItem,
+            uploadUrl: url
+        })
     }  
 }
 
@@ -62,4 +74,12 @@ async function groupExists(groupId){
         }
     }).promise()
     return result.Item
+}
+
+function getSignedUrl(imageId) {
+    return s3.getSignedUrl('putObject', {
+        Bucket: bucketName,
+        Key: imageId,
+        Expires: 300
+    })
 }
