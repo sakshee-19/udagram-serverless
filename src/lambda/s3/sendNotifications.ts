6603fa29ed9a4a7a127f0af9ccc3d489fc8924a1
saksjain <saksjain@adobe.com>
2020-05-23 01:07:33 +0530

    sns connect with s3

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import {S3Event, S3Handler} from 'aws-lambda'
+import {S3Event, SNSHandler, SNSEvent} from 'aws-lambda'
 import 'source-map-support'
 import * as AWS from 'aws-sdk'
 
@@ -14,11 +14,23 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
 
-// for this to connect to lambda add func without event in serverless.yaml the add permission and notif config
-export const handler: S3Handler = async(event: S3Event) => { // while upload to s3 bucket it should hit here
-    //S3Event have list of records
 
-    for(const record of event.Records) {
+export const handler:SNSHandler = async(SnsEvent: SNSEvent) => {
+    console.log("processing message ", JSON.stringify(SnsEvent))
+
+    for(const snsRecords of SnsEvent.Records) {
+        const snsMsg = snsRecords.Sns.Message
+        console.log("Processing s3 event ", snsMsg)
+        const s3Evnt = JSON.parse(snsMsg)
+        
+        await processS3Event(s3Evnt)
+    }
+}
+// for this to connect to lambda add func without event in serverless.yaml the add permission and notif config
+// export const handler: S3Handler = async(event: S3Event) => { // while upload to s3 bucket it should hit here
+//S3Event have list of records
+async function processS3Event(S3Event: S3Event){
+    for(const record of S3Event.Records) {
         const key = record.s3.object.key;
         console.log("Processing S3 item with key ",key);
     
